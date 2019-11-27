39143359e252f253303d860e758101ae2e5788a9
daniel.gomez <dgomezpascual@gmail.com>
2019-11-27 13:49:12 +0100

    Zebra_1

--- libreriapruebas/src/main/java/es/icp/libreriapruebas/Init.java
+++ libreriapruebas/src/main/java/es/icp/libreriapruebas/Init.java
@@ -1,6 +1,15 @@
 package es.icp.libreriapruebas;
 
+import com.zebra.rfid.api3.Readers;
+
 public class Init {
 
     public String test(){return "Hola mundo";}
+
+
+    public void testZebra()
+    {
+        Readers readers = new Readers();
+        test();
+    }
 }
